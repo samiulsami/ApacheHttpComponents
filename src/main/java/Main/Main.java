cde719cf9881279f4b8480aed6751119cf2db7b2
sami <sami@appscode.com>
2023-12-04 14:46:41 +0600

    created request format according to JMAP specs

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,5 +1,11 @@
 package Main;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
@@ -16,6 +22,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -24,7 +31,7 @@ public class Main {
             UriHttpRequestHandlerMapper mapper = new UriHttpRequestHandlerMapper();
             mapper.register("/pushListener", new pushListener());
             HttpServer server = ServerBootstrap.bootstrap()
-                    .setListenerPort(3323)
+                    .setListenerPort(3332)
                     .setHandlerMapper(mapper).create();
 
             server.start();
@@ -46,17 +53,83 @@ public class Main {
 
             switch(request.getRequestLine().getMethod()) {
                 case "POST": {
-                    String requestBody = ((HttpEntityEnclosingRequest)request).getEntity().toString();
-                    String responseBody = "test";
-                    responseBody = InputStreamToString(((HttpEntityEnclosingRequest) request).getEntity().getContent());
+                    String responseBody = "";
+                    String requestBody = InputStreamToString(((HttpEntityEnclosingRequest) request).getEntity().getContent());
+                    int responseCode = HttpStatus.SC_OK;
+                    //System.out.println(requestBody);
+                    try{
+                        ObjectMapper objectMapper = new ObjectMapper();
+                        var mp = objectMapper.readValue(requestBody, new TypeReference<HashMap<String, String>>() {});
+                        String pushSubscriptionId = mp.get("pushSubscriptionId");
+                        String verificationCode = mp.get("verificationCode");
+                       // System.out.println(pushSubscriptionId + " " + verificationCode);
+                        updatePushSubscription(pushSubscriptionId, verificationCode);
+                    }
+                    catch (Exception e){
+                        e.printStackTrace();
+                        responseCode = HttpStatus.SC_UNPROCESSABLE_ENTITY;
+                    }
+                    //responseBody = requestBody;
                     StringEntity entity = new StringEntity(responseBody, ContentType.TEXT_PLAIN);
-                    response.setStatusCode(HttpStatus.SC_OK);
+                    response.setStatusCode(responseCode);
                     response.setEntity(entity);
                     break;
                 }
                 default:
                     response.setStatusCode(HttpStatus.SC_METHOD_NOT_ALLOWED);
             }
+        }
+        /*
+        {
+              "using": [ "urn:ietf:params:jmap:core", "urn:ietf:params:jmap:mail"],
+              "methodCalls": [
+                [ "PushSubscription/set", {
+                        "update": {
+                            "3b1249a4-e800-4dba-97e1-6ba60be1ce9b": {
+                                 "verificationCode":"6e579a03-7e15-4af1-925e-04494312248a"
+                                }
+                        }
+                    }, "0" ]
+              ]
+            }
+         */
+        private static void updatePushSubscription(String pushSubscriptionId, String verificationCode)throws Exception{
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode root = mapper.createObjectNode();
+
+            ObjectNode verificationCodeNode = mapper.createObjectNode();
+            verificationCodeNode.put("verificationCode", verificationCode);
+
+            ObjectNode pushSubscriptionIdNode = mapper.createObjectNode();
+            pushSubscriptionIdNode.set(pushSubscriptionId, verificationCodeNode);
+
+            ObjectNode updateNode = mapper.createObjectNode();
+            updateNode.set("update", pushSubscriptionIdNode);
+
+            ArrayNode methodCallsNode = mapper.createArrayNode();
+            methodCallsNode.add("PushSubscription/set");
+            methodCallsNode.add(updateNode);
+            methodCallsNode.add("0");
+
+            ArrayNode methodCallsArrayNode = mapper.createArrayNode();
+            methodCallsArrayNode.add(methodCallsNode);
+
+            //ObjectNode methodCallsObjectNode = mapper.createObjectNode();
+            //methodCallsObjectNode.set("methodCalls", methodCallsArrayNode);
+
+            ArrayNode usingArrayNode = mapper.createArrayNode();
+            usingArrayNode.add("urn:ietf:params:jmap:core");
+            usingArrayNode.add("urn:ietf:params:jmap:mail");
+
+            //ObjectNode usingObjectNode = mapper.createObjectNode();
+           // usingObjectNode.set("using", usingNode);
+
+            ((ObjectNode)root).set("using",usingArrayNode);
+            ((ObjectNode)root).set("methodCalls",methodCallsArrayNode);
+
+            String json = null;
+            json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(root);
+            System.out.println(json);
         }
     }
 
